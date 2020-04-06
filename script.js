756994a42ad40518423cfc0967197bc7fe3462cc
wolel <56022602+wolel@users.noreply.github.com>
2020-04-06 21:35:37 +0200

    horologe with random color modify

--- script.js
+++ script.js
@@ -1,72 +1,37 @@
-window.onload = function () { //Il faut pense à attendre que la page soit chargée avant d'exécuter le script
+//animation des secondes
+const secondDiv = document.querySelector('.second-needle');
+const minDiv = document.querySelector('.min-needle');
+const hourDiv = document.querySelector('.hour-needle');
 
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secDeg = ((seconds/60)*360 + 90);
+    //déterminer la position des secondes
+    //comme nous avons mis nos div à transform = rotate(90deg), l'aiguille des seconde
+    secondDiv.style.transform = `rotate(${secDeg}deg)`;// changer la proprieté rotate
 
-var d = new Date('09 dec 2019');
-var time = d.getSeconds() + 60 * d.getMinutes() + 360 * d.getHours();
+    const min = now.getMinutes();
+    const minDeg =  ((min/60)*360) + ((seconds/60)*6) + 90;
+    //ou sont les seconde dans la minutes ((seconds/60)*6)
+    minDiv.style.transform = `rotate(${minDeg}deg)`;
 
-var aigS = document.getElementById('aigS');
-var aigM = document.getElementById('aigM');
-var aigH = document.getElementById('aigH');
+    const hour = now.getHours();
+    const hourDeg = ((hour/12)*360) + ((min/60)*30) + 90;
+    hourDiv.style.transform = `rotate(${hourDeg}deg)`
+}
+setInterval(setDate, 1000);
 
-aigS.style.transform = 'rotate( ' + (time * 6) + 'deg)';
-aigM.style.transform = 'rotate(' + Math.round(time / 10) + 'deg)';
-aigH.style.transform = 'rotate(' + Math.round(time /120) + 'deg)';
-
-setInterval(function () {
-    time++;
-    aigS.style.transform = 'rotate( ' + (time * 6) + 'deg)';
-    aigM.style.transform = 'rotate(' + Math.round(time / 10) + 'deg)';
-    aigH.style.transform = 'rotate(' + Math.round(time /120) + 'deg)';
-}, 1000);
-};
-
-//------------------------------------------
+//==========================================Change the color
 
 
 var btn1 = document.getElementById('btn1');
 var btn = document.getElementById('btn');
 
-
-/**function getColor() {
+function changeBackgroundColor() {
+    var backgr = document.getElementsByTagName('body');
     var x = Math.floor(Math.random()* 256);
     var y = Math.floor(Math.random()* 256);
     var z = Math.floor(Math.random()* 256);
-    var chColor = "rgb(" + x + "," + y + ","+ z + ")";
-}*/
-
-function changeHorlogeColor() {
-    var horloge = document.getElementById('horloge');
-    var x = Math.floor(Math.random()* 256);
-    var y = Math.floor(Math.random()* 256);
-    var z = Math.floor(Math.random()* 256);
-    var chColor = "rgb(" + x + "," + y + "," + z +")";
-    horloge.style.backgroundColor = chColor;
+    document.body.style.backgroundColor = "rgb(" + x + "," + y + "," + z + ")";
 }
- function changeBackgroundColor() {
-     var backgr = document.getElementsByTagName('body');
-     var x = Math.floor(Math.random()* 256);
-     var y = Math.floor(Math.random()* 256);
-     var z = Math.floor(Math.random()* 256);
-     var chColor1 = "rgb(" + x + "," + y + "," + z +")";
-     document.body.style.backgroundColor = chColor1;
- }
-
-var ladate = new Date();
-document.write("Heure brute : ");
-document.write(ladate.getHours()+":"+ ladate.getMinutes()+":"+ ladate.getSeconds());
-
-document.write("<BR>");
-
-var h = ladate.getHours();
-if (h < 10) {h = "0" + h}
-var m=ladate.getMinutes();
-if (m < 10) {m = "0" + m}
-var s = ladate.getSeconds();
-if ( s <10) {s = "0" + s}
-document.write("Heure formatée : ");
-document.write(h +":"+ m +":"+ s);
-
-
-
-
-
